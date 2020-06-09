c296f046cc9b6bd9f9597f11fb16ee8a5b1c9c45
tangpeng <961624856@qq.com>
2020-06-09 15:08:06 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-container>
+    <el-header>头</el-header>
+    <el-container>
+      <el-aside width="200px">左边</el-aside>
+      <el-main>内容</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
